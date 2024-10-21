a29c68989c85adce80f122a72c5d9a7e0ba2fd97
efes <efesjotaseles@gmail.com>
2024-10-21 20:38:54 -0300

    Creación de módulos y carpetas varias

---
+++ src/app/visualization/visualization.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VisualizationRoutingModule } from './visualization-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VisualizationRoutingModule
+  ]
+})
+export class VisualizationModule { }
