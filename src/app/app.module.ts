36f7dfc28d4f6641c0e4cc64428d7a48d60831fc
efes <efesjotaseles@gmail.com>
2024-10-28 20:26:04 -0300

    Merge con main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
-import { FormsModule } from '@angular/forms';
 import { TmdbService } from './services/tmdb.service';
 import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { SeriesComponent } from './components/series/series.component';
-import { LoginComponent } from './components/login/login.component';
 import { BusquedaComponent } from './components/busqueda/busqueda.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
