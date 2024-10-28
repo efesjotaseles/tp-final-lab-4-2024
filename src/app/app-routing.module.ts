6295387b8774567cce6b8fdeca0ae24e6f0727f7
Santiprog123 <santi-doussine@hotmail.com>
2024-10-28 01:20:58 -0300

    Navbar, componentes, rutas
    Agregue la navbar y la conecte con rutas a los nuevos componentes (inicio, peliculas, series y login)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { HomeComponent } from './components/home/home.component';
+import { SeriesComponent } from './components/series/series.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'peliculas',
+    component: PeliculasComponent
+  },
+  {
+    path: 'series',
+    component: SeriesComponent
+  },
+  {
+    path: 'iniciar-sesion',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { SeriesComponent } from './components/series/series.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PeliculasComponent,
+    SeriesComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
